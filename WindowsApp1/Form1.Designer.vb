804d889e8a163e10e4fef754961b7d598f1381ce
Spencer Allegrezza <sallegrezz@gmail.com>
2018-01-19 22:20:29 -0600

    Add project files.

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtFutureValue = New System.Windows.Forms.TextBox()
+        Me.txtInterestRate = New System.Windows.Forms.TextBox()
+        Me.txtYears = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.radSilver = New System.Windows.Forms.RadioButton()
+        Me.radGold = New System.Windows.Forms.RadioButton()
+        Me.listRequiredInvestment = New System.Windows.Forms.ListBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(20, 60)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(20, 144)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(20, 223)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(77, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(20, 297)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(77, 25)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Label4"
+        '
+        'txtFutureValue
+        '
+        Me.txtFutureValue.Location = New System.Drawing.Point(125, 60)
+        Me.txtFutureValue.Name = "txtFutureValue"
+        Me.txtFutureValue.Size = New System.Drawing.Size(100, 31)
+        Me.txtFutureValue.TabIndex = 4
+        '
+        'txtInterestRate
+        '
+        Me.txtInterestRate.Location = New System.Drawing.Point(125, 144)
+        Me.txtInterestRate.Name = "txtInterestRate"
+        Me.txtInterestRate.Size = New System.Drawing.Size(100, 31)
+        Me.txtInterestRate.TabIndex = 5
+        '
+        'txtYears
+        '
+        Me.txtYears.Location = New System.Drawing.Point(125, 223)
+        Me.txtYears.Name = "txtYears"
+        Me.txtYears.Size = New System.Drawing.Size(100, 31)
+        Me.txtYears.TabIndex = 6
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButton3)
+        Me.GroupBox1.Controls.Add(Me.radSilver)
+        Me.GroupBox1.Controls.Add(Me.radGold)
+        Me.GroupBox1.Location = New System.Drawing.Point(271, 41)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(376, 213)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Membership"
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(41, 152)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(173, 29)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "RadioButton3"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'radSilver
+        '
+        Me.radSilver.AutoSize = True
+        Me.radSilver.Location = New System.Drawing.Point(41, 105)
+        Me.radSilver.Name = "radSilver"
+        Me.radSilver.Size = New System.Drawing.Size(173, 29)
+        Me.radSilver.TabIndex = 1
+        Me.radSilver.TabStop = True
+        Me.radSilver.Text = "RadioButton2"
+        Me.radSilver.UseVisualStyleBackColor = True
+        '
+        'radGold
+        '
+        Me.radGold.AutoSize = True
+        Me.radGold.Location = New System.Drawing.Point(41, 60)
+        Me.radGold.Name = "radGold"
+        Me.radGold.Size = New System.Drawing.Size(173, 29)
+        Me.radGold.TabIndex = 0
+        Me.radGold.TabStop = True
+        Me.radGold.Text = "RadioButton1"
+        Me.radGold.UseVisualStyleBackColor = True
+        '
+        'listRequiredInvestment
+        '
+        Me.listRequiredInvestment.FormattingEnabled = True
+        Me.listRequiredInvestment.ItemHeight = 25
+        Me.listRequiredInvestment.Location = New System.Drawing.Point(25, 342)
+        Me.listRequiredInvestment.Name = "listRequiredInvestment"
+        Me.listRequiredInvestment.Size = New System.Drawing.Size(291, 154)
+        Me.listRequiredInvestment.TabIndex = 8
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(25, 572)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 9
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(150, 572)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 10
+        Me.btnReset.Text = "Button2"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(271, 572)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 11
+        Me.btnExit.Text = "Button3"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1243, 834)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.listRequiredInvestment)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.txtYears)
+        Me.Controls.Add(Me.txtInterestRate)
+        Me.Controls.Add(Me.txtFutureValue)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtFutureValue As TextBox
+    Friend WithEvents txtInterestRate As TextBox
+    Friend WithEvents txtYears As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents radSilver As RadioButton
+    Friend WithEvents radGold As RadioButton
+    Friend WithEvents listRequiredInvestment As ListBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Function PresentValue(futureValue As Integer, years As Integer, rate As Decimal) As Integer
+        Dim intPresentValue As Integer
+        intPresentValue = futureValue / ((1 + (rate / 100)) ^ years)   'Math
+        Return intPresentValue
+    End Function
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'Declare variables
+        Dim decGoldRate As Decimal = 0.03         'Rate from Gold membership
+        Dim decSilverRate As Decimal = 0.02       'Rate from Silver membership
+        Dim decBronzeRate As Decimal = 0.01       'Rate from Bronze membership
+
+
+    End Sub
+End Class
